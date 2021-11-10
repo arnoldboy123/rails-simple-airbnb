3c11447b46247c698fbfe831cdff3a79d8914e7a
arnold ho <tchoarnold@gmail.com>
2021-11-10 18:36:00 +0000

    I got the CSS to work and I am pretty proud of the results

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+4.times do
+  Flat.create!(
+  name: Faker::Lorem.word,
+  address: Faker::Address.street_address,
+  description: Faker::Lorem.sentence,
+  price_per_night: (0..100).to_a.sample,
+  number_of_guests: (0..5).to_a.sample
+  )
+end
